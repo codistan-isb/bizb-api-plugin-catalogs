3e2703640872b4dcefe1f71048b34828d4103a52
RAMZAN494 <bhutta69316@gmail.com>
2024-11-21 12:54:29 +0500

    Feat:CataLogItems_Resolved

--- src/queries/catalogItems.js
+++ src/queries/catalogItems.js
@@ -1,18 +1,109 @@
 import _ from "lodash";
 import ReactionError from "@reactioncommerce/reaction-error";
 
-/**
- * @name catalogItems
- * @method
- * @memberof Catalog/NoMeteorQueries
- * @summary query the Catalog by shop ID and/or tag ID
- * @param {Object} context - an object containing the per-request state
- * @param {Object} params - request parameters
- * @param {String[]} [params.searchQuery] - Optional text search query
- * @param {String[]} [params.shopIds] - Shop IDs to include (OR)
- * @param {String[]} [params.tags] - Tag IDs to include (OR)
- * @returns {Promise<MongoCursor>} - A MongoDB cursor for the proper query
- */
+// /**
+//  * @name catalogItems
+//  * @method
+//  * @memberof Catalog/NoMeteorQueries
+//  * @summary query the Catalog by shop ID and/or tag ID
+//  * @param {Object} context - an object containing the per-request state
+//  * @param {Object} params - request parameters
+//  * @param {String[]} [params.searchQuery] - Optional text search query
+//  * @param {String[]} [params.shopIds] - Shop IDs to include (OR)
+//  * @param {String[]} [params.tags] - Tag IDs to include (OR)
+//  * @returns {Promise<MongoCursor>} - A MongoDB cursor for the proper query
+//  */
+// export default async function catalogItems(
+//   context,
+//   {
+//     searchQuery,
+//     priceRange,
+//     shopIds,
+//     tagIds,
+//     catalogBooleanFilters,
+//     catalogSimpleFilters,
+//   } = {}
+// ) {
+//   const { collections } = context;
+//   const { Catalog, Tags } = collections;
+
+//   if ((!shopIds || shopIds.length === 0) && (!tagIds || tagIds.length === 0)) {
+//     throw new ReactionError(
+//       "invalid-param",
+//       "You must provide tagIds or shopIds or both"
+//     );
+//   }
+
+//   const query = {
+//     "product.isDeleted": { $ne: true },
+//     "product.isSoldOut": { $ne: true },
+//     ...catalogBooleanFilters,
+//     ...catalogSimpleFilters,
+//     "product.isVisible": true,
+//     "product.pricing.USD.minPrice": {
+//       $ne: null,
+//     },
+//     "product.pricing.USD.maxPrice": {
+//       $ne: null,
+//     },
+//     "product.media": {
+//       $elemMatch: {
+//         URLs: { $exists: true, $ne: null, $ne: "", $ne: {} },
+//       },
+//     },
+//     $and: [
+//       {
+//         "product.media.URLs.large": {
+//           $not: { $regex: /public\/bizb-\d+\/\/.*/ },
+//         },
+//       },
+//       {
+//         "product.media.URLs.medium": {
+//           $not: { $regex: /public\/bizb-\d+\/\/.*/ },
+//         },
+//       },
+//       {
+//         "product.media.URLs.small": {
+//           $not: { $regex: /public\/bizb-\d+\/\/.*/ },
+//         },
+//       },
+//       {
+//         "product.media.URLs.original": {
+//           $not: { $regex: /public\/bizb-\d+\/\/.*/ },
+//         },
+//       },
+//     ],
+//   };
+
+//   if (shopIds) query.shopId = { $in: shopIds };
+//   console.log("tags here in catalog", tagIds);
+//   if (tagIds) {
+
+
+//     query["product.tagIds"] = { $in: tagIds };
+//   }
+
+//   if (searchQuery) {
+//     query.$text = {
+//       $search: _.escapeRegExp(searchQuery),
+//     };
+//   }
+//   if (priceRange) {
+//     const minPrice = priceRange.find((item) => item.name === "minPrice").value;
+//     const maxPrice = priceRange.find((item) => item.name === "maxPrice").value;
+
+//     query[`product.pricing.USD.minPrice`] = { $gte: parseFloat(minPrice) };
+//     query[`product.pricing.USD.maxPrice`] = { $lte: parseFloat(maxPrice) };
+
+//   }
+
+
+//   return Catalog.find(query);
+// }
+
+
+
+
 export default async function catalogItems(
   context,
   {
@@ -25,7 +116,7 @@ export default async function catalogItems(
   } = {}
 ) {
   const { collections } = context;
-  const { Catalog, Tags } = collections;
+  const { Catalog, SimpleInventory } = collections;
 
   if ((!shopIds || shopIds.length === 0) && (!tagIds || tagIds.length === 0)) {
     throw new ReactionError(
@@ -33,75 +124,38 @@ export default async function catalogItems(
       "You must provide tagIds or shopIds or both"
     );
   }
-  // const query = {
-  //   "product.isDeleted": { $ne: true },
-  //   ...catalogBooleanFilters,
-  //   ...catalogSimpleFilters,
-  //   "product.isVisible": true,
-  //   "product.pricing.USD.minPrice": {
-  //     $ne: null,
-  //   },
-  //   "product.pricing.USD.maxPrice": {
-  //     $ne: null,
-  //   },
-  //   "product.media": {
-  //     $elemMatch: {
-  //       URLs: { $exists: true, $ne: null, $ne: "", $ne: {} },
-  //     },
-  //   },
-  // };
+
+  // Fetching inventory items with stock greater than zero
+  const inventoryIdsWithStock = await SimpleInventory.find({
+    inventoryInStock: { $gt: 0 }
+  }, {
+    projection: { "productConfiguration.productId": 1 }
+  }).toArray();
+
+
+  const productIds = inventoryIdsWithStock.map(item => item.productConfiguration.productId);
+
+
+
+
+  // Building the query for the Catalog
   const query = {
+    "product._id": { $in: productIds },
     "product.isDeleted": { $ne: true },
+    "product.isSoldOut": { $ne: true },
     ...catalogBooleanFilters,
     ...catalogSimpleFilters,
     "product.isVisible": true,
-    "product.pricing.USD.minPrice": {
-      $ne: null,
-    },
-    "product.pricing.USD.maxPrice": {
-      $ne: null,
-    },
-    "product.media": {
-      $elemMatch: {
-        URLs: { $exists: true, $ne: null, $ne: "", $ne: {} },
-      },
-    },
-    $and: [
-      {
-        "product.media.URLs.large": {
-          $not: { $regex: /public\/bizb-\d+\/\/.*/ },
-        },
-      },
-      {
-        "product.media.URLs.medium": {
-          $not: { $regex: /public\/bizb-\d+\/\/.*/ },
-        },
-      },
-      {
-        "product.media.URLs.small": {
-          $not: { $regex: /public\/bizb-\d+\/\/.*/ },
-        },
-      },
-      {
-        "product.media.URLs.original": {
-          $not: { $regex: /public\/bizb-\d+\/\/.*/ },
-        },
-      },
-    ],
+    "product.pricing.USD.minPrice": { $ne: null },
+    "product.pricing.USD.maxPrice": { $ne: null }
   };
 
-  if (shopIds) query.shopId = { $in: shopIds };
-  console.log("tags here in catalog", tagIds);
+  if (shopIds) {
+    query.shopId = { $in: shopIds };
+  }
   if (tagIds) {
-    // const tagDocuments = await Tags.find({ slug: { $in: tagIds } }).toArray();
-    // console.log("TAG DOCUMENT", tagDocuments);
-
-    // const tagIdsSlug = tagDocuments.map((tag) => tag._id);
-    // console.log("tags here in catalog slug", tagIds, tagIdsSlug, tagDocuments);
-
     query["product.tagIds"] = { $in: tagIds };
   }
-
   if (searchQuery) {
     query.$text = {
       $search: _.escapeRegExp(searchQuery),
@@ -110,18 +164,11 @@ export default async function catalogItems(
   if (priceRange) {
     const minPrice = priceRange.find((item) => item.name === "minPrice").value;
     const maxPrice = priceRange.find((item) => item.name === "maxPrice").value;
-    // const searchQuery1 = `${minPrice} ${maxPrice}`;
 
     query[`product.pricing.USD.minPrice`] = { $gte: parseFloat(minPrice) };
     query[`product.pricing.USD.maxPrice`] = { $lte: parseFloat(maxPrice) };
-    // query["product.pricing.USD"] = { $gte: parseInt(minPrice), $lte: parseInt(maxPrice) };
-    // query["pricing"] = { $gte: minPrice };
-    // query["pricing"] = { $lte: maxPrice };
-    // query.$text = {
-    //   $search: _.escapeRegExp(searchQuery1),
-    // };
   }
-  // const valueInter = await Catalog.find(query).toArray();
 
-  return Catalog.find(query);
+  return Catalog.find(query)
 }
+
